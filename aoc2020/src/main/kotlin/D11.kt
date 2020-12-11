7249c80f22ffb1eaae4215b043dc7c1e96a9194f
Yong Huang <yong.huang@fortum.com>
2020-12-11 23:50:00 +0100

    Aoc 2020 Day11 refactoring.

--- aoc2020/src/main/kotlin/D11.kt
+++ aoc2020/src/main/kotlin/D11.kt
@@ -6,15 +6,15 @@ class D11 {
     }
 
     fun getSolution2(): Int {
-        return getSolution(5, true)
+        return getSolution(5, false)
     }
 
-    private fun getSolution(maxOccupied: Int = 4, ignoreFloor: Boolean = false): Int {
+    private fun getSolution(maxOccupied: Int = 4, evaluateFloor: Boolean = true): Int {
         var current = getInputs()
-        val state = State(true)
-        while (state.isNotStable) {
-            state.isNotStable = false
-            current = applyRules(current, state, maxOccupied, ignoreFloor)
+        val state = State(false)
+        while (!state.isStable) {
+            state.isStable = true
+            current = applyRules(current, state, maxOccupied, evaluateFloor)
         }
         return getOccupied(current)
     }
@@ -23,49 +23,52 @@ class D11 {
         current: List<CharArray>,
         state: State,
         maxOccupied: Int,
-        ignoreFloor: Boolean
+        evaluateFloor: Boolean
     ): List<CharArray> {
         val after = current.map { it.copyOf() }
-        for (i in current.indices) {
-            for (j in current[i].indices) {
-                when (current[i][j]) {
-                    Pos.FLOOR.rep -> continue
-                    Pos.EMPTY.rep -> if (shouldTake(current, i to j, ignoreFloor)) {
-                        after[i][j] = Pos.OCCUPIED.rep
-                        state.isNotStable = true
+
+        for (row in current.indices) {
+            for (col in current[row].indices) {
+                when (current[row][col]) {
+                    Pos.FLOOR.mark -> continue
+                    Pos.EMPTY.mark -> if (shouldTake(current, row to col, evaluateFloor)) {
+                        after[row][col] = Pos.OCCUPIED.mark
+                        state.isStable = false
                     }
-                    Pos.OCCUPIED.rep -> if (shouldLeave(current, i to j, maxOccupied, ignoreFloor)) {
-                        after[i][j] = Pos.EMPTY.rep
-                        state.isNotStable = true
+                    Pos.OCCUPIED.mark -> if (shouldLeave(current, row to col, maxOccupied, evaluateFloor)) {
+                        after[row][col] = Pos.EMPTY.mark
+                        state.isStable = false
                     }
                 }
             }
         }
+
         return after
     }
 
-    private fun shouldTake(current: List<CharArray>, pos: Pair<Int, Int>, ignoreFloor: Boolean): Boolean {
-        return getSurroundings(current, pos, ignoreFloor).count { it == Pos.OCCUPIED.rep } == 0
+    private fun shouldTake(current: List<CharArray>, pos: Pair<Int, Int>, evaluateFloor: Boolean): Boolean {
+        return getSurroundings(current, pos, evaluateFloor).count { it == Pos.OCCUPIED.mark } == 0
     }
 
     private fun shouldLeave(
         current: List<CharArray>,
         pos: Pair<Int, Int>,
         maxOccupied: Int,
-        ignoreFloor: Boolean
+        evaluateFloor: Boolean
     ): Boolean {
-        return getSurroundings(current, pos, ignoreFloor).count { it == Pos.OCCUPIED.rep } >= maxOccupied
+        return getSurroundings(current, pos, evaluateFloor).count { it == Pos.OCCUPIED.mark } >= maxOccupied
     }
 
-    private fun getSurroundings(current: List<CharArray>, pos: Pair<Int, Int>, ignoreFloor: Boolean): List<Char?> {
-        val up = getPosMark(current, pos.first to pos.second, -1 to 0, ignoreFloor)
-        val upRight = getPosMark(current, pos.first to pos.second, -1 to 1, ignoreFloor)
-        val right = getPosMark(current, pos.first to pos.second, 0 to 1, ignoreFloor)
-        val downRight = getPosMark(current, pos.first to pos.second, 1 to 1, ignoreFloor)
-        val down = getPosMark(current, pos.first to pos.second, 1 to 0, ignoreFloor)
-        val downLeft = getPosMark(current, pos.first to pos.second, 1 to -1, ignoreFloor)
-        val left = getPosMark(current, pos.first to pos.second, 0 to -1, ignoreFloor)
-        val upLeft = getPosMark(current, pos.first to pos.second, -1 to -1, ignoreFloor)
+    private fun getSurroundings(current: List<CharArray>, pos: Pair<Int, Int>, evaluateFloor: Boolean): List<Char?> {
+        val up = getPosMark(current, pos.first to pos.second, -1 to 0, evaluateFloor)
+        val upRight = getPosMark(current, pos.first to pos.second, -1 to 1, evaluateFloor)
+        val right = getPosMark(current, pos.first to pos.second, 0 to 1, evaluateFloor)
+        val downRight = getPosMark(current, pos.first to pos.second, 1 to 1, evaluateFloor)
+        val down = getPosMark(current, pos.first to pos.second, 1 to 0, evaluateFloor)
+        val downLeft = getPosMark(current, pos.first to pos.second, 1 to -1, evaluateFloor)
+        val left = getPosMark(current, pos.first to pos.second, 0 to -1, evaluateFloor)
+        val upLeft = getPosMark(current, pos.first to pos.second, -1 to -1, evaluateFloor)
+
         return listOf(up, upRight, right, downRight, down, downLeft, left, upLeft)
     }
 
@@ -73,15 +76,15 @@ class D11 {
         current: List<CharArray>,
         pos: Pair<Int, Int>,
         direction: Pair<Int, Int>,
-        ignoreFloor: Boolean
+        evaluateFloor: Boolean
     ): Char? {
         var newPos = pos.first + direction.first to pos.second + direction.second
         var mark = getPosMark(current, newPos)
-        if (!ignoreFloor) {
+        if (evaluateFloor) {
             return mark
         }
 
-        while (mark == Pos.FLOOR.rep) {
+        while (mark == Pos.FLOOR.mark) {
             newPos = newPos.first + direction.first to newPos.second + direction.second
             mark = getPosMark(current, newPos)
         }
@@ -89,14 +92,14 @@ class D11 {
     }
 
     private fun getPosMark(current: List<CharArray>, pos: Pair<Int, Int>): Char? {
-        val col = current[0].size
-        val row = current.size
-        return if (pos.first < 0 || pos.first >= row || pos.second < 0 || pos.second >= col) null
+        val rowSize = current.size
+        val colSize = current[0].size
+        return if (pos.first < 0 || pos.first >= rowSize || pos.second < 0 || pos.second >= colSize) null
         else current[pos.first][pos.second]
     }
 
     private fun getOccupied(current: List<CharArray>): Int {
-        return current.map { line -> line.count { it == Pos.OCCUPIED.rep } }.sum()
+        return current.map { line -> line.count { it == Pos.OCCUPIED.mark } }.sum()
     }
 
     private fun getInputs(): List<CharArray> {
@@ -104,11 +107,6 @@ class D11 {
     }
 }
 
-enum class Pos(val rep: Char) {
-    FLOOR('.'),
-    OCCUPIED('#'),
-    EMPTY('L')
-}
+enum class Pos(val mark: Char) { FLOOR('.'), OCCUPIED('#'), EMPTY('L') }
 
-data class State(var isNotStable: Boolean)
-
+data class State(var isStable: Boolean)
